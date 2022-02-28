fcb9358af001292146e96f32f998d6239909e589
Davi Lacerda <dlacerda9@gmail.com>
2022-02-28 15:07:21 -0300

    Revert "Update useCart.tsx"
    This reverts commit 7a3fe73752707f0d90af61a0537a777131f8f917.

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -34,54 +34,11 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
 
   const addProduct = async (productId: number) => {
     try {
-      let product = await api.get(`/products/${productId}`).then((response) => response.data)
-
-      const productAmount = cart.reduce((acc, product) => {
-        if(product.id === productId) {
-          acc += product.amount
-        }
-
-        return acc;
-      }, 1);
-
-      if(productAmount === 1){
-        const newProduct = {
-          ...product,
-          amount: 1
-        }
-        setCart([...cart,newProduct])
-        localStorage.setItem('@RocketShoes:cart', JSON.stringify([...cart,newProduct]))
-      } else {
-        const newCart = cart.map((item) => {
-          if(item.id === productId){
-            return {...item, amount: item.amount + 1}
-          }
-          return item
-        })
-        setCart(newCart)
-        localStorage.setItem('@RocketShoes:cart', JSON.stringify(newCart))
-      }
-
-      // const storagedCart = localStorage.getItem('@RocketShoes:cart')
-      // if(storagedCart){
-      //   const storagedCardObject = JSON.parse(storagedCart)
-      //   const newCart = storagedCardObject.map((currentProduct:Product) => {
-      //     if(productId === currentProduct.id){
-      //       const newProduct = {...product, amount: (currentProduct.amount + 1) || 1}
-      //       updateProductAmount(newProduct)
-      //       return newProduct 
-      //     }
-      //   })
-      //   setCart(newCart)
-      //   localStorage.setItem('@RocketShoes:cart', JSON.stringify([...newCart]))
-      // } else {
-      //   console.log(2);
-        
-      //   updateProductAmount(product)
-      //   setCart(product)
-      //   localStorage.setItem('@RocketShoes:cart', JSON.stringify([...product]))
-      // }
-      console.log(localStorage.getItem('@RocketShoes:cart'));
+      let response = await api.get(`/stock/${productId}`)
+      let productAmount = response.data.amount
+      let newProductAmount = productAmount - 1
+      updateProductAmount()
+      
       
       
     } catch {
@@ -93,7 +50,7 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     try {
       // TODO
     } catch {
-      toast.error('Erro ao tentar remover o produto do carrinho!')
+      // TODO
     }
   };
 
@@ -102,33 +59,12 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     amount,
   }: UpdateProductAmount) => {
     try {
-      let response = await api.get(`/stock/${productId}`)
-      let stock = response.data.amount
-
-      if(amount <= 0){
-        return;
-      }
-
-      else if(amount > stock){
-        toast.error('Quantidade solicitada fora de estoque');
-        return
-      }
-
-      const newCart = cart.map((product) => {
-        if(productId === product.id){
-          return {
-            ...product,
-            amount: stock.amount
-          }
-        } else {
-          return product
-        }
-      })  
-      
-      setCart(newCart)
-      localStorage.setItem('@RocketShoes:cart', JSON.stringify([...cart]))
+      const update = await api.post(`/stock/${productId}`, {
+        productId, 
+        amount,
+      })
     } catch {
-      toast.error('Erro na alteração de quantidade do produto');
+      // TODO
     }
   };
 
